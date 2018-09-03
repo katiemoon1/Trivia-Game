d28fbb2c47761c2a3275c76e1d1803d5334efb4e
Moon Child <kaitline.moon@gmail.com>
2018-09-03 07:07:38 -0400

    Updated variables and functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(document).ready(() => {
+
 // Setting up an array of objects that contains the questions for the quiz
 var questions = [
     {
@@ -22,11 +24,34 @@ var questions = [
     }
 ]
 
-// Setting the player's score to 0
+// Setting up the variables for the game
 var playerScore = 0;
-
-// Setting the player's guess
 var playerGuess;
+var correctAnswers = [];
+var questionTotal = 5;
+var showQuestions;
+
+
+
+// Creating a function to hide the questions and timer when the page loads
+var hideGame = function() {
+    $(".question").hide();
+}
+
+// Creating a function that will show the questions and timer when the "let's go!" button is clicked
+var showGame = function() {
+    $(".btn").on("click", function() {
+        $(".btn").hide();
+        $(".question").show();
+    })
+
+}
+
+// Creating a function that will load the questions
+var loadQuestions = function() {
+    
+}
+
 
 // Creating a function to display the questions and loop through the questions array
 var startQuiz = function() {
@@ -38,3 +63,9 @@ var startQuiz = function() {
         }
     }
 }
+
+// Calling functions
+hideGame();
+showGame();
+
+});
