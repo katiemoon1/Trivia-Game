fe197bcfbda0ada90e3de1bcbf62ec206a70870c
Moon Child <kaitline.moon@gmail.com>
2018-09-04 17:23:13 -0400

    Changed my HTML back to only having one  question div and JS file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,34 +5,33 @@ var questions = [
     {
         question: "What trail do you need to hike to get to Precipice Lake?",
         choices: ["John Muir Trail", "High Sierra Trail", "Pacific Crest Trail"],
-        answer: 2
+        answer: 1
     },
     {
         question: "In what park can you find Longs Peak?",
         choices: ["Rocky Mountain National Park", "Badlands National Park", "Glacier National Park"],
-        answer: 1
+        answer: 0
     },
     {
         question: "What river flows through the Grand Canyon?",
         choices: ["Mississippi River", "Youghiogheny River", "Colorado River"],
-        answer: 3
+        answer: 2
     },
     {
         question: "Which National Park gets the most visitors each year?",
         choices: ["Grand Canyon National Park", "Great Smoky Mountains National Park", "Yosemite National Park"],
-        answer: 2
+        answer: 1
     },
     {
         question: "How many National Parks are in the United States?",
         choices: ["34", "59", "78"],
-        answer: 2
+        answer: 1
     }
 ];
 
 // Setting up the variables for the game
 var playerScore = 0;
-var playerGuess;
-var questionTotal = 5;
+var questionTotal = 0;
 var showTimer;
 var totalTime = 20;
 
@@ -47,13 +46,20 @@ var decrement = function() {
     if (totalTime === 0) {
         stop();
         alert("Your time is up!");
-        $("#final-score").text("Your score is: " + playerScore / questionTotal);
+        showScore();
     }
 };
 
 var stop = function() {
     clearInterval(showTimer);
 };
+
+// Display the score
+var showScore = function() {
+    if (totalTime === 0) {
+    $("#final-score").text("Your score is: " + playerScore);
+    }
+}
 
 // Creating a function to hide the questions and timer when the page loads
 var hideGame = function() {
@@ -74,24 +80,26 @@ var showGame = function() {
 
 // Creating a function to display the questions and loop through the questions array
 var startQuiz = function() {
-    $("#game-question1").text(questions[0].question);
-    $("#game-question2").text(questions[1].question);
-    $("#game-question3").text(questions[2].question);
-    $("#game-question4").text(questions[3].question);
-    $("#game-question5").text(questions[4].question);
-    for (var i = 0; i < questions.length; i++) {
-        $("input[value='a']").text(questions[0].choices);
-        $("input[name='two']").text(questions[1].choices);
-        $("input[name='three']").text(questions[2].choices);
-        $("input[name='four']").text(questions[3].choices);
-        $("input[name='five']").text(questions[4].choices);
-        if (playerGuess === questions[i].answer) {
-            playerScore++
-        } else {
-
+    if (questionTotal < questions.length) {
+        $("#game-questions").empty();
+        $("#game-question").text(questions[questionTotal].question);
+        $("#choices").empty();
+        for (var i = 0; i < questions[questionTotal].choices.length; i++) {
+           $("#choices").append("<input type='radio' name='choice' class='choice-button' number='" + i + "'>" + questions[questionTotal].choices[i] + "</input>");     
         }
+    } else {
+
     }
 };
+
+// Click handler to check if the answer selected is correct
+$(document).on("click", ".choice-button", function() {
+    if ($(this).attr("number") === questions[questionTotal].answer) {
+        playerScore++;
+    }
+    questionTotal++;
+    startQuiz();
+});
 
 // Calling functions
 hideGame();
